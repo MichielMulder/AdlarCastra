04056605f8c30a64509c844fb2121e6844ffa206
MichielMulder <107243649+MichielMulder@users.noreply.github.com>
2025-01-23 11:19:57 +0100

    Create config_flow.py

---
+++ custom_components/adlar/config_flow.py
@@ -0,0 +1,148 @@
+"""Config flow for Adlar Castra Aurora 2 integration."""
+
+from __future__ import annotations
+
+import logging
+from typing import Any, Optional
+
+from pymodbus.client import AsyncModbusSerialClient, AsyncModbusTcpClient
+from pymodbus.exceptions import ModbusException
+import voluptuous as vol
+
+from homeassistant import config_entries
+
+from .const import DOMAIN
+from .device import DEVICE_TYPES
+from .registers import HoldingRegisters
+
+STEP_TCP_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required("name", default="AdlarCastra"): str,
+        vol.Required("host_ip"): str,
+        vol.Required("host_port", default="502"): str,
+        vol.Required("slave_id", default=1): int,
+    }
+)
+
+STEP_SERIAL_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required("name", default="AdlarCastra"): str,
+        vol.Required("host_port"): str,
+        vol.Required("slave_id", default=1): int,
+    }
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_validate_device(com_type, port, slave_id, address: str | None) -> None:
+    """Validate device model."""
+    if com_type == "tcp":
+        client = AsyncModbusTcpClient(
+            address,
+            port=port,
+        )
+    else:
+        client = AsyncModbusSerialClient(
+            method="rtu",
+            port=port,
+            stopbits=1,
+            bytesize=8,
+            parity="E",
+            baudrate=19200,
+            timeout=1,
+        )
+    try:
+        await client.connect()
+        result = await client.read_holding_registers(
+            HoldingRegisters.control_type, count=1, slave=int(slave_id)
+        )
+    except ModbusException as value_error:
+        client.close()
+        raise ValueError("cannot_connect") from value_error
+    if hasattr(result, "message"):
+        client.close()
+        raise ValueError("invalid_response")
+    if len(result.registers) == 0:
+        client.close()
+        raise ValueError("invalid_response")
+    value_output = int.from_bytes(
+        result.registers[0].to_bytes(2, "little", signed=False),
+        "little",
+        signed=False,
+    )
+    if value_output not in DEVICE_TYPES:
+        _LOGGER.debug(
+            "Device Type %s not found in supported devices list",
+            str(value_output),
+        )
+        raise ValueError("unsupported_device")
+    client.close()
+
+
+class AdlarConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Adlar Castra Modbus TCP."""
+
+    VERSION = 3
+
+    data: Optional[dict(str, Any)]
+
+    async def async_step_user(self, user_input: Optional[dict(str, Any)] = None):
+        """Invoke when a user initiates a flow via the user interface."""
+        return await self.async_step_menu(user_input)
+
+    async def async_step_menu(self, user_input: Optional[dict(str, Any)] = None):
+        """Show Communications Selection."""
+        return self.async_show_menu(
+            step_id="menu",
+            menu_options=["tcp", "serial"],
+            # description_placeholders={
+            #     "model": "Example model",
+            # },
+        )
+
+    async def async_step_tcp(self, user_input: Optional[dict(str, Any)] = None):
+        """Configure ModBus TCP entry."""
+        errors: dict(str, str) = {}
+
+        if user_input is not None:
+            try:
+                await async_validate_device(
+                    "tcp",
+                    user_input["host_port"],
+                    user_input["slave_id"],
+                    user_input["host_ip"],
+                )
+            except ValueError as error:
+                errors["base"] = str(error)
+            if not errors:
+                # Input is valid, set data.
+                self.data = user_input
+                self.data.update({"com_type": "tcp"})
+                self.data.update({"board_type": "AURORA"})
+                return self.async_create_entry(title=user_input["name"], data=self.data)
+        return self.async_show_form(
+            step_id="tcp", data_schema=STEP_TCP_DATA_SCHEMA, errors=errors
+        )
+
+    async def async_step_serial(self, user_input: Optional[dict(str, Any)] = None):
+        """Configure ModBus Serial RTU entry."""
+        errors: dict(str, str) = {}
+
+        if user_input is not None:
+            try:
+                await async_validate_device(
+                    "serial", user_input["host_port"], user_input["slave_id"], None
+                )
+            except ValueError as error:
+                errors["base"] = str(error)
+            if not errors:
+                # Input is valid, set data.
+                self.data = user_input
+                self.data.update({"com_type": "serial"})
+                self.data.update({"host_ip": None})
+                self.data.update({"board_type": "AURORA"})
+                return self.async_create_entry(title=user_input["name"], data=self.data)
+        return self.async_show_form(
+            step_id="serial", data_schema=STEP_SERIAL_DATA_SCHEMA, errors=errors
+        )
